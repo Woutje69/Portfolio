013948f684489c4d33438ccb8574c00873b0820d
woodyvelde <woutvandevelde@hotmail.com>
2026-01-06 16:32:58 +0100

    labo05plesk

---
+++ labo05/formchecking_thanks.php
@@ -0,0 +1,57 @@
+<?php
+
+	$name = isset($_GET['name']) ? $_GET['name'] : false;
+	$age = isset($_GET['age']) ? $_GET['age'] : false;
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Testform</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+
+<?php
+
+
+		echo '<p>Thank you, stranger</p>';
+
+
+?>
+
+	<div id="debug">
+
+<?php
+
+	/**
+	 * Helper Functions
+	 * ========================
+	 */
+
+		/**
+		 * Dumps a variable
+		 * @param mixed $var
+		 * @return void
+		 */
+		function dump($var) {
+			echo '<pre>';
+			var_dump($var);
+			echo '</pre>';
+		}
+
+
+	/**
+	 * Main Program Code
+	 * ========================
+	 */
+
+		// dump $_GET
+		dump($_GET);
+
+?>
+
+	</div>
+
+</body>
+</html>
---
+++ labo05/mymessages.php
@@ -0,0 +1,63 @@
+<?php
+
+// Constanten (connectie-instellingen databank)
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'woodyvelde');
+define('DB_PASS', 'woodyvelde123');
+define('DB_NAME', 'labo05-database');
+
+
+date_default_timezone_set('Europe/Brussels');
+
+// Verbinding maken met de databank
+try {
+    $db = new PDO('mysql:host=' . DB_HOST .';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Verbindingsfout: ' .  $e->getMessage();
+    exit;
+}
+
+// Opvragen van alle taken uit de tabel tasks
+$stmt = $db->prepare('SELECT * FROM messages ORDER BY added_on DESC');
+$stmt->execute();
+$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+?><!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="styles.css"/>
+
+    <title>Mijn berichten</title>
+</head>
+<body>
+    <?php if (sizeof($items) > 0) { ?>
+    <ul>
+
+    </ul>
+
+    <table>
+        <thead>
+            <th>e-mail</th>
+            <th>berichten</th>
+            <th>tijd</th>
+        </thead>
+        <tbody>
+            <?php foreach ($items as $item) { ?>
+                <tr>
+                    <td><?php echo $item['sender']; ?>: </td>
+                    <td><?php echo $item['message']; ?> </td>
+                    <td>(<?php echo (new Datetime($item['added_on']))->format('d-m-Y H:i:s'); ?>)</td>
+                </tr>
+            <?php } ?>
+
+        </tbody>
+    </table>
+    <?php
+    } else {
+        echo '<p>Nog geen berichten ontvangen.</p>' . PHP_EOL;
+    }
+    ?>
+</body>
