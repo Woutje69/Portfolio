56e0d26895657fc5439dcf265b8e369dd2455353
woodyvelde <woutvandevelde@hotmail.com>
2026-01-09 18:43:25 +0100

    nu is normaal alles gefixt

--- contact/formchecking_thanks.php
+++ contact/formchecking_thanks.php
@@ -11,14 +11,46 @@
 	<link rel="stylesheet" type="text/css" href="./form-styles.css" />
 </head>
 <body>
-
-<?php
-
-
-		echo '<p>Thank you, '$name'</p>';
-
-
-?>
+	<header>
+            <div class="container">
+                <a href="./"><img src="./images/logo-wout-vandevelde.png" alt="logo wout vandevelde" class="logo"></a>
+                <nav>
+                    <ul>
+                        <li><a href="./">Home</a></li>
+                        <li><a href="./exercises/">Exercises</a></li>
+                        <li><a href="./about-me/">About me</a></li>
+                        <li><a href="./contact/">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </header>
+	<main>
+		<?php
+		echo "<p>Thank you, '$name'</p>";
+		?>
+	</main>
+	<footer>
+            <div class="container">
+                <section>
+                    <h2>You can also find me on</h2>
+                    <ul>
+                        <li>
+                            <a href="https://www.linkedin.com/in/wout-vandevelde-3999a3371/" target="_blank"><img src="./images/icons/icons8-linkedin-100.png" alt="logo linkedin"></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/wout_vandevelde_/" target="_blank"><img src="./images/icons/icons8-instagram-100.png" alt="logo instagram"></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/wout.vandevelde.75" target="_blank"><img src="./images/icons/icons8-facebook-100.png" alt="logo facebook"></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Woutje69" target="_blank"><img src="./images/icons/icons8-github-100.png" alt="logo github"></a>
+                        </li>
+                    </ul>
+                    <p class="copyright">&copy; 2025 Wout Vandevelde - copyright</p>
+                </section>
+            </div>
+    </footer>
 
 	<div id="debug">
 
