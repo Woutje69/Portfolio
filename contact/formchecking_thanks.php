c7042a3ebd5c8a7477308d5f1d9f92bc21300403
woodyvelde <woutvandevelde@hotmail.com>
2026-01-09 18:45:50 +0100

    en nu ?

--- contact/formchecking_thanks.php
+++ contact/formchecking_thanks.php
@@ -7,19 +7,24 @@
 <html lang="en">
 <head>
 	<title>Bedankt!</title>
-	<meta charset="UTF-8" />
-	<link rel="stylesheet" type="text/css" href="./form-styles.css" />
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../styles/normalize.css">
+        <link rel="stylesheet" type="text/css" href="../styles/main-styles.css">
 </head>
 <body>
 	<header>
             <div class="container">
-                <a href="./"><img src="./images/logo-wout-vandevelde.png" alt="logo wout vandevelde" class="logo"></a>
+                <a href="../"><img src="../images/logo-wout-vandevelde.png" alt="logo wout vandevelde" class="logo"></a>
                 <nav>
                     <ul>
-                        <li><a href="./">Home</a></li>
-                        <li><a href="./exercises/">Exercises</a></li>
-                        <li><a href="./about-me/">About me</a></li>
-                        <li><a href="./contact/">Contact</a></li>
+                        <li><a href="../">Home</a></li>
+                        <li><a href="../exercises/">Exercises</a></li>
+                        <li><a href="../about-me/">About me</a></li>
+                        <li><a href="../contact/">Contact</a></li>
                     </ul>
                 </nav>
             </div>
@@ -35,55 +40,21 @@
                     <h2>You can also find me on</h2>
                     <ul>
                         <li>
-                            <a href="https://www.linkedin.com/in/wout-vandevelde-3999a3371/" target="_blank"><img src="./images/icons/icons8-linkedin-100.png" alt="logo linkedin"></a>
+                            <a href="https://www.linkedin.com/in/wout-vandevelde-3999a3371/" target="_blank"><img src="../images/icons/icons8-linkedin-100.png" alt="logo linkedin"></a>
                         </li>
                         <li>
-                            <a href="https://www.instagram.com/wout_vandevelde_/" target="_blank"><img src="./images/icons/icons8-instagram-100.png" alt="logo instagram"></a>
+                            <a href="https://www.instagram.com/wout_vandevelde_/" target="_blank"><img src="../images/icons/icons8-instagram-100.png" alt="logo instagram"></a>
                         </li>
                         <li>
-                            <a href="https://www.facebook.com/wout.vandevelde.75" target="_blank"><img src="./images/icons/icons8-facebook-100.png" alt="logo facebook"></a>
+                            <a href="https://www.facebook.com/wout.vandevelde.75" target="_blank"><img src="../images/icons/icons8-facebook-100.png" alt="logo facebook"></a>
                         </li>
                         <li>
-                            <a href="https://github.com/Woutje69" target="_blank"><img src="./images/icons/icons8-github-100.png" alt="logo github"></a>
+                            <a href="https://github.com/Woutje69" target="_blank"><img src="../images/icons/icons8-github-100.png" alt="logo github"></a>
                         </li>
                     </ul>
                     <p class="copyright">&copy; 2025 Wout Vandevelde - copyright</p>
                 </section>
             </div>
     </footer>
-
-	<div id="debug">
-
-<?php
-
-	/**
-	 * Helper Functions
-	 * ========================
-	 */
-
-		/**
-		 * Dumps a variable
-		 * @param mixed $var
-		 * @return void
-		 */
-		function dump($var) {
-			echo '<pre>';
-			var_dump($var);
-			echo '</pre>';
-		}
-
-
-	/**
-	 * Main Program Code
-	 * ========================
-	 */
-
-		// dump $_GET
-		dump($_GET);
-
-?>
-
-	</div>
-
 </body>
 </html>
