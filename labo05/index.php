3f1fd2eaf6819dee26675ae8686e8e0b0920b2d8
woodyvelde <woutvandevelde@hotmail.com>
2026-01-09 10:34:52 +0100

    fix styles koppelen

--- labo05/index.php
+++ labo05/index.php
@@ -67,7 +67,7 @@ if (isset($_POST['btnSubmit'])) {
 <head>
     <title>Testform</title>
     <meta charset="UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="../styles.css"/>
+    <link rel="stylesheet" type="text/css" href="./styles.css"/>
 </head>
 <body>
 
