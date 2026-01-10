a150ac3f13848ebe969d80eb026306589bb2f82b
woodyvelde <woutvandevelde@hotmail.com>
2026-01-10 12:54:35 +0100

    test stijlen

--- contact/formchecking_thanks.php
+++ contact/formchecking_thanks.php
@@ -6,7 +6,7 @@
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Bedankt!</title>
+	<title>Thank You!!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -35,7 +35,7 @@
 			<?php
 			echo "<h1>Thank you, {$name}</h1>";
 			?>
-			<img src="../images/thank-you.jpg" alt="foto bedankt voor de feedback">
+			<img src="../images/thank-you.jpg" alt="foto bedankt voor de feedback" class="thank-you-img">
 		</div>
 	</main>
 	<footer>
